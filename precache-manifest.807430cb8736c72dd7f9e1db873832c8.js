c6d3a26ec07fed9c92bef05c75f721e1ca44783b
ulyavrubel <uvrubel@gmail.com>
2020-02-05 15:49:52 +0800

    Updates

---
+++ precache-manifest.807430cb8736c72dd7f9e1db873832c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "469e94ad5ad95a58aaa860d5ca6ac43c",
+    "url": "/pomodoroClock/index.html"
+  },
+  {
+    "revision": "00c4cce2b90f6e68b15f",
+    "url": "/pomodoroClock/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "be5e969fffb34454ee21",
+    "url": "/pomodoroClock/static/js/2.b27f2596.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pomodoroClock/static/js/2.b27f2596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c4cce2b90f6e68b15f",
+    "url": "/pomodoroClock/static/js/main.8f7dcbb8.chunk.js"
+  },
+  {
+    "revision": "ae6ae398916dfca41079",
+    "url": "/pomodoroClock/static/js/runtime-main.e10dde1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoroClock/static/media/logo.5d5d9eef.svg"
+  }
+]);
